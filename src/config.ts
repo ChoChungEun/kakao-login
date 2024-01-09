430171c63943e8192c6f7f2ea18e6b32d1e71048
ChoChungEun <82curst@gmail.com>
2024-01-09 16:22:51 +0900

    Fix

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const REST_API_KEY = "e9a48a5c635a16ec516b530f89cfb50a";
-// export const REDIRECT_URI = "http://localhost:5173/auth/kakao/callback";
-export const REDIRECT_URI =
-  "https://chochungeun.github.io/kakao-login/auth/kakao/callback";
+export const REDIRECT_URI = "http://localhost:5173/auth/kakao/callback";
+// export const REDIRECT_URI =
+//   "https://chochungeun.github.io/kakao-login/auth/kakao/callback";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 export const CLIENT_SECRET = "fWI8V2c3yuSNZUgIcrMDgIrJULJhLzwI";
