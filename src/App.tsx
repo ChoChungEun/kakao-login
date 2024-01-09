a941c5e174dd9fe14f6297e9fa4ea3d3ac7b0ead
ChoChungEun <82curst@gmail.com>
2024-01-09 16:48:41 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
@@ -15,7 +10,6 @@ function App() {
         <Route path="/kakao-login" element={<Home />} />
         <Route path="/auth/kakao/callback" element={<Auth />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
       </Routes>
     </Router>
   );
