797ec5e2033b01651ff839f34c0e5b696d14dc6e
ChoChungEun <82curst@gmail.com>
2024-01-09 15:48:44 +0900

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <div>kakao login</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/kakao/callback" element={<Auth />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
