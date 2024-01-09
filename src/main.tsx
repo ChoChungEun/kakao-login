b2ab9fc73b20f50a17bcfbe86aaf5f866bfd2c49
ChoChungEun <82curst@gmail.com>
2024-01-09 17:31:44 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-// import App from "./App.tsx";
-// import { RecoilRoot } from "recoil";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <RecoilRoot>
-//     <App />
-//   </RecoilRoot>
-// );
-
-const router = createBrowserRouter([
-  {
-    path: "/kakao-login",
-    element: <Home />,
-  },
-]);
+import App from "./App.tsx";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,17 +1,18 @@
 import { useNavigate } from "react-router-dom";
-// import { useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { useEffect } from "react";
 import axios from "axios";
 import qs from "qs";
-// import { isLogin } from "../atoms/login";
+import { isLogin } from "../atoms/login";
 import { CLIENT_SECRET, REDIRECT_URI, REST_API_KEY } from "../config";
 
 const Auth = () => {
   const navigate = useNavigate();
-  // const setIsLoggedIn = useSetRecoilState(isLogin);
+  const setIsLoggedIn = useSetRecoilState(isLogin);
 
   const params = new URL(document.URL).searchParams;
   const code = params.get("code");
+  console.log("code", code);
 
   const getToken = async () => {
     const payload = qs.stringify({
@@ -31,7 +32,7 @@ const Auth = () => {
       window.Kakao.init(REST_API_KEY);
       // access token 설정
       window.Kakao.Auth.setAccessToken(res.data.access_token);
-      // setIsLoggedIn(true);
+      setIsLoggedIn(true);
       navigate("/profile");
     } catch (err) {
       console.log(err);
