b2fee2d7910c1fed713e91738bed884b83344434
ChoChungEun <82curst@gmail.com>
2024-01-09 16:43:00 +0900

    Fix Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/kakao-login" element={<Home />} />
+        <Route path="/auth/kakao/callback" element={<Auth />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
       </Routes>
     </Router>
   );
